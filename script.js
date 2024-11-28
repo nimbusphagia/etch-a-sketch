928bcafd7f6ac968c2cd5acf4ddb228597a9fb1a
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2024-11-27 23:02:06 -0500

    RESIZE GRID

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //CREATE GRID
 const container = document.querySelector("#container");
-
-function createDivs (amount){
+let initialGridSize = 16;
+function createGrid (amount){
     for (i = 0; i < (amount * amount); i++){
         let square = document.createElement("div");
+        let width = Math.floor(100 / amount);
         square.className = "square";
         container.appendChild(square);
+        square.style.width = `${width}%`;
+        square.style.height = `${width}%`;
     }
 }
-
-createDivs(16);
+createGrid(initialGridSize);
 
 //HOVER
 function changeColor (event){
@@ -28,5 +30,28 @@ function stopColor () {
 container.addEventListener("mousedown", startColor);
 container.addEventListener("mouseup", stopColor);
 container.addEventListener("mouseleave", stopColor);
+
+//USER INPUT
+/* 
+1. Button calls prompt to get new grid size value
+2. Delete previous grid to then createGrid() with the new value, change gridSize text
+3. fit the grid with CSS
+
+*/
+
+const btnResize = document.querySelector("button");
+
+/*document.querySelectorAll(".square").forEach((delete) => {
+    delete.remove();
+  });*/
+
+function promptSize (){
+    container.innerHTML = "";
+    let newSize = prompt("Enter new grid size:", "");
+    newSize;
+    createGrid(newSize);
+}
+btnResize.addEventListener("click", promptSize);
+
 
 
